91af8e832b1908af6febec49bf50c340a8bace32
Djoy César Klein <djoy_cesar@hotmail.com>
2025-07-02 01:34:25 -0300

    Aulas 25-28 completas(curso Reactjs e Nodejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -18,6 +19,27 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput labelText='task' id='meuInput' type='text' placeholder='Digite algo' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
